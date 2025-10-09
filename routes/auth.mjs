d234161621a299b26c80e242bfe03084d35ca94c
Ewan Williams <ewan@ewan.name>
2025-10-09 10:57:31 +0100

    Login route implemented

--- routes/auth.mjs
+++ routes/auth.mjs
@@ -1,37 +1,57 @@
 import express from "express";
 import User from "../models/user.mjs";
 import { generateTokenAndSetCookie } from "../utils/generateToken.mjs";
+import bcrypt from 'bcrypt';
 
 
 const router = express.Router();
 
 
-// TODO USER LOGIN/REGISTER/PROFILE UPDATE ROUTES
+// USER REGISTRATION ROUTE
 router.post('/register', async (req, res) => {
     try {
-        const existingUser = await User.findOne({username: req.body.username});
+        // check username is unique
+        const existingUser = await User.findOne({ username: req.body.username });
         if (existingUser) {
             res.status(400).json({error: "username already in use"});
         } else {
+            // create new user
             const user = await User.create({
                 username: req.body.username,
                 password: req.body.password
             });
-            // set login cookie
+            // set login cookie and response
             generateTokenAndSetCookie(user._id, res);
             res.status(201).json({message: "user created successfully"});
         }
-    }
-    catch(err) {
+    } catch(err) {
         console.error("Registration error: ", err);
         res.status(500).json({error: err});
     }
 });
 
 
+// USER LOGIN ROUTE
+router.post('/login', async (req, res) => {
+    try {
+        const userFound = await User.findOne({ username: req.body.username });
+        if (userFound) {
+            const passwordCorrect =  await bcrypt.compare(req.body.password, userFound.password);
 
-// login route
-router.post('/login', (req, res, next) => {
+            if (passwordCorrect) {
+                generateTokenAndSetCookie(userFound._id, res);
+                res.status(200).json({ message: "Logged in!" });
+            } else {
+                res.status(400).json({ error: "Invalid username or password." });
+            }
+
+        } else {
+            res.status(400).json({ error: "Invalid username or password." });
+        }
+    } catch(err) {
+        console.error("Login error: ", err);
+        res.status(500).json({error: err});
+    }
 });
 
 export default router;
