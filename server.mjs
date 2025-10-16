58ba0ce14f167f20980fd5fcfcec7954b8edffe1
kaceylaird <6lairk77@solent.ac.uk>
2025-10-16 17:22:48 +0100

    .env file added and descriptions taken out of browse page. server.mjs file reverted back to original as .env file is added

--- server.mjs
+++ server.mjs
@@ -9,7 +9,7 @@ import authRoutes from './routes/auth.mjs';
 import recipeRoutes from './routes/recipe.mjs';
 
 const app = express();
-const PORT = process.env.PORT || 5173;
+const PORT = process.env._PORT;
 const DB_URL = process.env.database_url;
 
 // middleware
