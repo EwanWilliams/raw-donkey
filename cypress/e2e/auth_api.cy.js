c8523b2ce3bd203f758e6eb6e7c179e043869428
Ewan Williams <ewan@ewan.name>
2025-11-03 13:45:40 +0000

    attempting to get tests to work on development version

--- cypress/e2e/auth_api.cy.js
+++ cypress/e2e/auth_api.cy.js
@@ -1,17 +1,19 @@
 describe('AUTH API Tests', () => {
-    const url = "http://localhost:4173";
-  beforeEach(() => {
-    cy.visit(url);
-    cy.clearCookies();
-  });
-  it('correct login', () => {
-    cy.request('POST', `${url}/api/auth/login`, {
-        username: "test_user",
-        password: "password1"
-    }).then((res) => {
-        expect(res.status).to.eq(200);
-        expect(res.body).to.have.property('message', 'logged in');
-        expect(res.headers).to.have.property('set-cookie');
+    before(() => {
+        cy.wait(10000);
     });
-  });
+    beforeEach(() => {
+        cy.visit("http://localhost:4173");
+        cy.clearCookies();
+    });
+    it('correct login', () => {
+        cy.request('POST', '/api/auth/login', {
+            username: "test_user",
+            password: "password1"
+        }).then((res) => {
+            expect(res.status).to.eq(200);
+            expect(res.body).to.have.property('message', 'logged in');
+            expect(res.headers).to.have.property('set-cookie');
+        });
+    });
 });
