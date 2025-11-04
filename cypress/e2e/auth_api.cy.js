bb6290e280c2a094defe1b3d5501a2d1ecc1d1ea
Ewan Williams <ewan@ewan.name>
2025-11-04 16:36:35 +0000

    unhappy login api test written

--- cypress/e2e/auth_api.cy.js
+++ cypress/e2e/auth_api.cy.js
@@ -7,13 +7,26 @@ describe('AUTH API Tests', () => {
         cy.clearCookies();
     });
     it('correct login', () => {
-        cy.request('POST', '/api/auth/login', {
-            username: "test_user",
-            password: "password1"
+        cy.request({
+            method: 'POST',
+            url: '/api/auth/login',
+            body: { username: "test_user", password: "password1" }
         }).then((res) => {
             expect(res.status).to.eq(200);
             expect(res.body).to.have.property('message', 'logged in');
             expect(res.headers).to.have.property('set-cookie');
+        });
+    });
+    it('incorrect login', () => {
+        cy.request({
+            method: 'POST',
+            url: '/api/auth/login',
+            body: { username: "test_user", password: "wrongpassword" },
+            failOnStatusCode: false
+        }).then((res) => {
+            expect(res.status).to.eq(400);
+            expect(res.body).to.have.property('error', 'Invalid username or password.');
+            expect(res.headers).to.not.have.property('set-cookie');
         });
     });
 });
