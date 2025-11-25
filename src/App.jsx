ae05e0da9924629ca4635caa71c28c493a7a1a29
kaceylaird <6lairk77@solent.ac.uk>
2025-11-25 14:08:24 +0000

    start of api call coded

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,15 @@ import Settings from "./pages/settings";
 
 export default function App() {
   
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  }
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  } 
 
   return (
     <Router>
