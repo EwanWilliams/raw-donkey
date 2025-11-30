fcaa00c68d4c96e8020dfb1eea4579dfd4cdac55
Ewan Williams <ewan@ewan.name>
2025-11-30 13:27:48 +0000

    vite istanbul setup correctly

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import istanbul from  'vite-plugin-istanbul';
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        istanbul({
+            cypress: true,
+            requireEnv: false,
+            forceBuildInstrument: true
+        })
+    ],
     server: {
         proxy: {
             '/api': {
